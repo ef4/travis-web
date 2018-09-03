9d05efed7aff5bc2d5a850b6d2f7205bdb6a7e34
Edward Faulkner <ef@alum.mit.edu>
2018-09-03 15:39:39 -0400

    fixup

--- vbuild.js
+++ vbuild.js
@@ -4,7 +4,7 @@ const { Webpack } = require('ember-webpack');
 
 let templateCompiler = function () {
   throw new Error('fixme: should be built into v2 app instead');
-}
+};
 
 let w = new Webpack(
   __dirname + '/vanilla-dist',
