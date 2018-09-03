de02bb43e0d1fcf698cb20b0114d2db21b491e11
Edward Faulkner <ef@alum.mit.edu>
2018-09-03 15:39:31 -0400

    fixup

--- vbuild.js
+++ vbuild.js
@@ -1,13 +1,18 @@
 /* eslint-env node */
+/* eslint-disable no-console */
 const { Webpack } = require('ember-webpack');
+
+let templateCompiler = function () {
+  throw new Error('fixme: should be built into v2 app instead');
+}
 
 let w = new Webpack(
   __dirname + '/vanilla-dist',
   __dirname + '/dist',
   templateCompiler,
-  consoleWrite(msg) {
+  function (msg) {
     console.log(msg);
   }
-});
+);
 
 w.build();
