17ea2bae627d7fba268bb5a04228c974542ff80d
Edward Faulkner <ef@alum.mit.edu>
2018-09-04 16:28:25 -0400

    progress

--- vbuild.js
+++ vbuild.js
@@ -2,14 +2,9 @@
 /* eslint-disable no-console */
 const { Webpack } = require('ember-webpack');
 
-let templateCompiler = function () {
-  throw new Error('fixme: should be built into v2 app instead');
-};
-
 let w = new Webpack(
-  __dirname + '/vanilla-dist',
+  __dirname + '/vanilla-dist/travis-web',
   __dirname + '/dist',
-  templateCompiler,
   function (msg) {
     console.log(msg);
   }
